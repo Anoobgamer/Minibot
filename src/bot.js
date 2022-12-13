a91b5a214d148dda9f05ef9ad3b0213745341cfc
Anoobgamer <118984619+Anoobgamer@users.noreply.github.com>
2022-12-13 16:57:31 -0500

    Add files via upload

---
+++ src/bot.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { token } = process.env;
+const { Collection, GatewayIntentBits, Client } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./src/functions/${folder}/${file}`)(client);
+}
+
+client.handleEvents();
+client.handleEvents();
+client.login(token);
